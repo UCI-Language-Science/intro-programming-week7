d979b2321c5cb6f3dd53e807811d0b4218f55882
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2024-08-26 17:00:09 -0700

    add tests

---
+++ library.py
@@ -0,0 +1,13 @@
+# Submission file for Week 7 Assignment
+
+class Book:
+
+    def __init__(self, *args, **kwargs,):
+        pass
+        #TODO: your implementation here
+
+class Library:
+
+    def __init__(self, *args, **kwargs,):
+        pass
+        #TODO: your implementation here
